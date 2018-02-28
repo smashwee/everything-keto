ca72aa0593f33cc6bd0b4e703082f053a66367e0
Ashley Cacoilo <acacoilo926@gmail.com>
2018-02-27 20:11:26 -0500

    added some info to sections and template for scrollTo in js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 console.log('JS is working')
+
+$(document).ready(function(){
+  
+  function scroll(top){
+    $('element').animate({
+      scrollTop: top
+    }, 500)
+  }
+
+  //get the top position of the element
+  var linkPos = $('#element-id').position().top;
+  $('element-id').on('click', function(){
+    scroll(linkPos)
+  })
+  //scroll to it
+
+  $('element-id').on('click,' function(){
+    scoll(0);
+  })
+})
