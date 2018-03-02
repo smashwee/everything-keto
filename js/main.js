a625c7aeb74efd95e855392137fdffd5960ed1fe
Ashley Cacoilo <acacoilo926@gmail.com>
2018-03-01 23:02:21 -0500

    added contact form to contact.html and updated css and other files

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
 console.log('JS is working')
 
+function submit(){
+    $('form').html('<h2>Thanks for submitting your question! I will do my best to answer you as soon as I can :)</h2>');
+}
+
 $(document).ready(function(){
 
+
   function scroll(top){
-    $('element').animate({
+    $('html, body').animate({
       scrollTop: top
     }, 500)
   }
 
   //get the top position of the element
-  var linkPos = $('#element-id').position().top;
-  $('element-id').on('click', function(){
-    scroll(linkPos) //invoke scroll function
+  // var linkPos = $('element-id').position().top;
+
+
+  $('#bottom').on('click', function(){
+    scroll(0) //invoke scroll function
   })
+
   //scroll to it
 
-  $('element-id').on('click,' function(){
-    scoll(0);
-  })
+  // $('element-id').on('click,' function(){
+  //  scoll(0);
+//  })
+
+
 })
